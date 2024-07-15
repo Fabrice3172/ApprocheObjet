f2e5696b7a7552f441196a5c927dc58a700f50b1
FM <test@toto.fr>
2024-07-15 16:44:09 +0200

    4

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte unCompte = new Compte(1234, Integer.MAX_VALUE);
+
+        System.out.println(unCompte);
+    }
+}
---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,28 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+    int numeroCompte;
+    int solde;
+
+    public Compte(int numeroCompte, int solde) {
+        this.numeroCompte = numeroCompte;
+        this.solde = solde;
+    }
+
+    //public int getNumeroCompte() {
+      //  return numeroCompte;
+    //}
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setNumeroCompte(int numeroCompte) {
+        this.numeroCompte = numeroCompte;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+}
